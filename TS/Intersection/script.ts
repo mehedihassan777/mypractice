f9df2fe893de29ff0a67f0b78cb2b30340ef8670
Mehedi Hassan <mehedi15-5815@diu.edu.bd>
2023-01-13 17:06:26 +0600

    Intersection Types

---
+++ TS/Intersection/script.ts
@@ -0,0 +1,35 @@
+interface BusinessPartner {
+    name: string;
+    credit: number;
+}
+
+interface Identity {
+    id: number;
+    name: string;
+}
+
+interface Contact {
+    email: string;
+    phone: string;
+}
+
+type Employee = Identity & Contact; //Intersect
+
+let e: Employee = {
+    id: 100,
+    name: 'John Doe',
+    email: 'john.doe@example.com',
+    phone: '(408)-897-5684'
+};
+
+type Customer = BusinessPartner & Contact; //Intersect
+
+let c: Customer = {
+    name: 'ABC Inc.',
+    credit: 1000000,
+    email: 'sales@abcinc.com',
+    phone: '(408)-897-5735'
+};
+
+console.log(e);
+console.log(c);
